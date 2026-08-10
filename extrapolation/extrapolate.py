84c98d1e588ab0458d930992ecde7542c8a770f9
Haobo Yan <wittscien@gmail.com>
2026-08-10 18:38:36 +0800

    Plot finite-a data in flow extrapolations

--- extrapolation/extrapolate.py
+++ extrapolation/extrapolate.py
@@ -125,8 +125,11 @@ def continuum_extrapolation(params, metadata, data, ensembles, fit_range, figdir
 
 def flow_extrapolation(continuum, fit_range, figdir):
     tech = 'bootstrap'
-    data_color = '#3C5488'
-    fit_color = '#E64B35'
+    # Colors and markers for the finite-a data, continuum limit, and flow fit
+    ensemble_color = {'cA211': '#4477AA', 'cB211': '#228833', 'cC211': '#CC6677'}
+    ensemble_marker = {'cA211': 'o', 'cB211': 's', 'cC211': '^'}
+    continuum_color = '#332288'
+    fit_color = '#EE7733'
     figdir = Path(figdir) / 'flow'
     figdir.mkdir(parents=True, exist_ok=True)
     result = {}
@@ -144,7 +147,32 @@ def flow_extrapolation(continuum, fit_range, figdir):
             result[k][moment] = {'tf': np.asarray(tf_list,dtype=int), 't_over_t0': x, 'samples': data_matrix, 'mean': mean, 'err': err, 'fit_indices': fit_index, 'fit': result_para}
 
             fig, ax = plt.subplots(figsize=(6.4, 4))
-            ax.errorbar(x,mean,yerr=err,ls='None',marker='o',color=data_color,mec=data_color,capsize=2,fillstyle='none')
+            data_fit_mask = np.zeros(len(tf_list),dtype=bool)
+            data_fit_mask[fit_index] = True
+            ensemble_mean = []
+            ensemble_err = []
+            # Plot the data before continuum extrapolation; fade points outside the flow fit range
+            for ensemble in continuum[k][moment][tf_list[0]]['ensembles']:
+                ensemble_x = []
+                ensemble_y = []
+                ensemble_yerr = []
+                for tf in tf_list:
+                    ensemble_index = continuum[k][moment][tf]['ensembles'].index(ensemble)
+                    ensemble_x.append(continuum[k][moment][tf]['t_over_t0_values'][ensemble_index])
+                    ensemble_y.append(continuum[k][moment][tf]['mean'][ensemble_index])
+                    ensemble_yerr.append(continuum[k][moment][tf]['err'][ensemble_index])
+                ensemble_x = np.asarray(ensemble_x)
+                ensemble_y = np.asarray(ensemble_y)
+                ensemble_yerr = np.asarray(ensemble_yerr)
+                ensemble_mean.append(ensemble_y)
+                ensemble_err.append(ensemble_yerr)
+                ax.errorbar(ensemble_x[~data_fit_mask],ensemble_y[~data_fit_mask],yerr=ensemble_yerr[~data_fit_mask],ls='None',marker=ensemble_marker[ensemble],markersize=4,color=ensemble_color[ensemble],mec=ensemble_color[ensemble],capsize=2,fillstyle='none',alpha=0.12,zorder=1)
+                ax.errorbar(ensemble_x[data_fit_mask],ensemble_y[data_fit_mask],yerr=ensemble_yerr[data_fit_mask],ls='None',marker=ensemble_marker[ensemble],markersize=4,color=ensemble_color[ensemble],mec=ensemble_color[ensemble],capsize=2,fillstyle='none',alpha=0.75,label=ensemble,zorder=2)
+            ensemble_mean = np.asarray(ensemble_mean)
+            ensemble_err = np.asarray(ensemble_err)
+            # Plot the continuum-extrapolated data used for the flow-time extrapolation
+            ax.errorbar(x[~data_fit_mask],mean[~data_fit_mask],yerr=err[~data_fit_mask],ls='None',marker='D',markersize=4.5,color=continuum_color,mec=continuum_color,capsize=2,fillstyle='none',alpha=0.18,zorder=2)
+            ax.errorbar(x[data_fit_mask],mean[data_fit_mask],yerr=err[data_fit_mask],ls='None',marker='D',markersize=4.5,color=continuum_color,mec=continuum_color,capsize=2,fillstyle='none',label=r'$a^2\to 0$',zorder=3)
             fit_x = np.unique(np.concatenate((np.linspace(0,max(x),100),[x[fit_index[0]],x[fit_index[-1]]])))
             recon_matrix = np.zeros([len(result_para['samples']),len(fit_x)])
             for ls in range(len(result_para['samples'])):
@@ -153,18 +181,20 @@ def flow_extrapolation(continuum, fit_range, figdir):
             fit_err = tp.cal_err(recon_matrix,tech)
             fit_mask = (fit_x >= x[fit_index[0]]) & (fit_x <= x[fit_index[-1]])
             ax.fill_between(fit_x,fit_mean-fit_err,fit_mean+fit_err,color=fit_color,alpha=0.20,edgecolor='none')
-            ax.fill_between(fit_x[fit_mask],fit_mean[fit_mask]-fit_err[fit_mask],fit_mean[fit_mask]+fit_err[fit_mask],color=fit_color,alpha=0.45,edgecolor='none')
-            ax.errorbar([0],[result_para['mean'][0]],yerr=[result_para['err'][0]],marker='s',markersize=6,color='k',capsize=2)
+            ax.fill_between(fit_x[fit_mask],fit_mean[fit_mask]-fit_err[fit_mask],fit_mean[fit_mask]+fit_err[fit_mask],color=fit_color,alpha=0.45,edgecolor='none',label='flow fit')
+            ax.errorbar([0],[result_para['mean'][0]],yerr=[result_para['err'][0]],marker='P',markersize=7,color='k',capsize=2,label=r'$t_f\to 0$',zorder=4)
             ax.set_xlabel(r'$t_f/t_0$')
             ax.set_ylabel(r'$\langle x^{%d}\rangle/\langle x\rangle$' % (moment - 1))
             ax.set_title(r'$%s\quad \mathrm{continuum}$' % inputs.labels(k))
-            ymin = min(result_para['mean'][0]-result_para['err'][0],mean[-1]-err[-1])
-            ymax = max(result_para['mean'][0]+result_para['err'][0],mean[-1]+err[-1])
+            # Set the y range from the fit region so noisy unused points do not compress the plot
+            ymin = min(np.min(ensemble_mean[:,fit_index]-ensemble_err[:,fit_index]),np.min(mean[fit_index]-err[fit_index]),np.min(fit_mean-fit_err),result_para['mean'][0]-result_para['err'][0])
+            ymax = max(np.max(ensemble_mean[:,fit_index]+ensemble_err[:,fit_index]),np.max(mean[fit_index]+err[fit_index]),np.max(fit_mean+fit_err),result_para['mean'][0]+result_para['err'][0])
             ypad = 0.05 * max(ymax - ymin, np.finfo(float).eps)
             ax.set_ylim([ymin - ypad, ymax + ypad])
             ax.tick_params(axis='both', direction='in')
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
+            ax.legend(frameon=False,ncol=2,fontsize=8)
             fig.tight_layout()
             fig.savefig(figdir / ('ratio_%s_moment%d.pdf' % (k,moment)),transparent=True)
             tp.show_in_spyder()
